40c0e54aa0002734df6a57b6fdaf88461c63fa79
lduboisd <lduboisd@inria.fr>
2021-04-15 15:25:09 +0200

    correction de la preuve de l'hypotèse ajoutée dans l'expansion

--- eta_expand.v
+++ eta_expand.v
@@ -48,13 +48,28 @@ let eq := eval cbv in (gen_eq l B t u)
 in run_template_program (tmUnquote eq) 
 ltac:(fun z => 
 let u := eval hnf in (z.(my_projT2)) 
-in assert u by reflexivity))))
+in assert u by (intros ; rewrite H; reflexivity)))))
 | _ => fail "not an equality"
 end.
 
+Goal False.
+get_def hd.
+eta_expand_hyp hd_def.
+
+Ltac expand_tuple p := 
+match p with
+| (?x, ?y) => let T := type of x in idtac x ; idtac y ; idtac T;
+ eta_expand_hyp x ; idtac 2 ; clear x ; idtac 3 ; expand_tuple y ; idtac 4
+| unit => idtac 5
+| ?y => fail 100 "Wrong parameter" y
+end.
 
 Ltac expand_fun f :=
-let H:= get_def_cont f in eta_expand_hyp H.
+let H:= get_def_cont f in eta_expand_hyp H ; clear H.
+
+Goal forall (A: Type) (l : list A) (a : A), hd a l = a -> tl l = [].
+
+get_definitions_cont ltac:(fun p => expand_tuple p).
 
 
 
