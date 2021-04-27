32629617c1601ba75259726746d59caa47d06f5c
lduboisd <lduboisd@inria.fr>
2021-04-27 16:28:04 +0200

    tentative de preuve

--- elimination_fixpoints.v
+++ elimination_fixpoints.v
@@ -32,27 +32,132 @@ MetaCoq Quote Recursively Definition length_reif_rec := Datatypes.length.
 
 Print length_reif_rec.
 
+Fixpoint eliminate_lambda_aux (t : term) (l : list (aname*term)) :=
+match t with 
+| tLambda na T u => eliminate_lambda_aux u ((na, T)::l)
+| _ => (t, l)
+end.
+
+Fixpoint eliminate_lambda t := eliminate_lambda_aux t [].
+
+Fixpoint create_lambda (t : term) (l : list (aname*term)) :=
+match l with 
+| nil => t
+| (na, T):: xs => create_lambda (tLambda na T t) xs
+end.
 
 
 (* Returns the definition in a fixpoint *)
-Fixpoint get_def_in_fix (f: term) := match f with 
-| tLambda na T u => get_def_in_fix u 
+Definition get_def_in_fix_aux  (f: term) := 
+let fix aux acc f :=
+match f with 
+| tLambda na T u => aux ((na, T)::acc) u 
 | tFix l _ => match l with 
-          | [] => f
-          | x :: xs => x.(dbody)
+          | [] => (acc, f)
+          | x :: xs => (acc, x.(dbody))
           end
-| _ => f
-end.
+| _ => (acc, f)
+end
+in aux [] f.
 
-MetaCoq Quote Definition term_test := (forall A (l : list A),
-    Datatypes.length l =
-    (fix length (s : list A) : nat :=
-       match s with
-       | nil => 0
-       | cons _ s' => S (Datatypes.length s')
-       end) l).
+Definition get_def_in_fix (f: term) := 
+let p := get_def_in_fix_aux f in
+create_lambda p.2 p.1.
+
+MetaCoq Quote Definition term_test := (Datatypes.length =
+             (fun A : Type =>
+              fix length (l : list A) : nat := match l with
+                                               | [] => 0
+                                               | _ :: l' => S (length l')
+                                               end)).
 
 Print term_test.
+
+(* tApp
+  (tInd
+     {|
+     inductive_mind := (MPfile ["Logic"%string; "Init"%string; "Coq"%string], "eq"%string);
+     inductive_ind := 0 |} [])
+  [tProd {| binder_name := nNamed "A"%string; binder_relevance := Relevant |}
+     (tSort (Universe.of_levels (inr (Level.Level "Coq.Init.Datatypes.63"))))
+     (tProd {| binder_name := nAnon; binder_relevance := Relevant |}
+        (tApp
+           (tInd
+              {|
+              inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string], "list"%string);
+              inductive_ind := 0 |} []) [tRel 0])
+        (tInd
+           {|
+           inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string], "nat"%string);
+           inductive_ind := 0 |} []));
+  tConst (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string], "length"%string) [];
+  tLambda {| binder_name := nNamed "A"%string; binder_relevance := Relevant |}
+    (tSort (Universe.of_levels (inr (Level.Level "TransfoCoq.elimination_fixpoints.236"))))
+    (tFix
+       [{|
+        dname := {| binder_name := nNamed "length"%string; binder_relevance := Relevant |};
+        dtype := tProd {| binder_name := nNamed "l"%string; binder_relevance := Relevant |}
+                   (tApp
+                      (tInd
+                         {|
+                         inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                           "list"%string);
+                         inductive_ind := 0 |} []) [tRel 0])
+                   (tInd
+                      {|
+                      inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                        "nat"%string);
+                      inductive_ind := 0 |} []);
+        dbody := tLambda {| binder_name := nNamed "l"%string; binder_relevance := Relevant |}
+                   (tApp
+                      (tInd
+                         {|
+                         inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                           "list"%string);
+                         inductive_ind := 0 |} []) [tRel 1])
+                   (tCase
+                      ({|
+                       inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                         "list"%string);
+                       inductive_ind := 0 |}, 1, Relevant)
+                      (tLambda {| binder_name := nNamed "l"%string; binder_relevance := Relevant |}
+                         (tApp
+                            (tInd
+                               {|
+                               inductive_mind := (MPfile
+                                                    ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                                 "list"%string);
+                               inductive_ind := 0 |} []) [tRel 2])
+                         (tInd
+                            {|
+                            inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                              "nat"%string);
+                            inductive_ind := 0 |} [])) (tRel 0)
+                      [(0,
+                       tConstruct
+                         {|
+                         inductive_mind := (MPfile ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                           "nat"%string);
+                         inductive_ind := 0 |} 0 []);
+                      (2,
+                      tLambda {| binder_name := nNamed "a"%string; binder_relevance := Relevant |}
+                        (tRel 2)
+                        (tLambda {| binder_name := nNamed "l'"%string; binder_relevance := Relevant |}
+                           (tApp
+                              (tInd
+                                 {|
+                                 inductive_mind := (MPfile
+                                                      ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                                   "list"%string);
+                                 inductive_ind := 0 |} []) [tRel 3])
+                           (tApp
+                              (tConstruct
+                                 {|
+                                 inductive_mind := (MPfile
+                                                      ["Datatypes"%string; "Init"%string; "Coq"%string],
+                                                   "nat"%string);
+                                 inductive_ind := 0 |} 1 []) [tApp (tRel 3) [tRel 0]])))]);
+        rarg := 0 |}] 0)] *)
 
 
 Definition body_reif := tLambda
@@ -238,7 +343,7 @@ in match t with *)
 
 
 MetaCoq Quote Definition test' := (Datatypes.length =
-             (fix length {A} (l : list A) : nat :=
+             (fun A => fix length (l : list A) : nat :=
                 match l with
                 | nil  => 0
                 | cons _ xs => S (length xs)
@@ -426,25 +531,13 @@ Compute (subst10 length_reif body_reif).
 MetaCoq Unquote Definition toto_unquote := toto.
 Print toto_unquote.
 
-Fixpoint eliminate_lambda_aux (t : term) (l : list (aname*term)) :=
-match t with 
-| tLambda na T u => eliminate_lambda_aux u ((na, T)::l)
-| _ => (t, l)
-end.
 
-Fixpoint eliminate_lambda t := eliminate_lambda_aux t [].
-
-Fixpoint create_lambda (t : term) (l : list (aname*term)) :=
-match l with 
-| nil => t
-| (na, T):: xs => create_lambda (tLambda na T t) xs
-end.
 
 
 Fixpoint replace_tFix_by_def (l : list term) (def : term) := match l with 
 | nil => nil
 | cons x xs => let p := (eliminate_lambda x) in match p.1 with 
-        | tFix _ _ => create_lambda (subst10 def (get_def_in_fix x)) p.2 :: xs
+        | tFix _ _ => (subst10 def (get_def_in_fix x)) :: xs
         | _ => x :: (replace_tFix_by_def xs def)
       end
 end.
@@ -478,8 +571,20 @@ in pose w))))
 | _ => fail "not an equality" 
 end.
 
+Goal Nat.add = (fun n m : nat => match n with
+                            | 0 => m
+                            | S p => S (p + m)
+                            end).
+Proof.
+unfold Nat.add.
+Abort.
+
 Goal False.
 get_def @Datatypes.length.
+get_def Nat.add.
+eliminate_fix_hyp add_def.
+assert P.
+unfold P. 
 Fail eliminate_fix_hyp length_def.
 
  expand_hyp length_def.
