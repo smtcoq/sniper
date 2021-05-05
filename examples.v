21e093c2e0d7b2960d336f633c504d7a26cf9930
Chantal Keller <Chantal.Keller@lri.fr>
2021-05-05 16:25:03 +0200

    No more problem with CompDec

--- examples.v
+++ examples.v
@@ -26,10 +26,9 @@ End Generic.
 Hypothesis length_app : forall A, forall (l1 l2: list A),
        (Z.of_nat #|l1 ++ l2| =? Z.of_nat #|l1| + Z.of_nat #|l2|).
 
-Lemma length_app_auto : forall B (HB: CompDec B), forall (l1 l2 l3 : list B), 
+Lemma length_app_auto : forall B (HB: CompDec B), forall (l1 l2 l3 : list B),
 ((length (l1 ++ l2 ++ l3)) =? (length l1 + length l2 + length l3))%nat.
-Proof. intros B HB l1 l2 l3. 
-snipe length_app. Qed.
+Proof. intros B HB l1 l2 l3. snipe length_app. Qed.
 
 
 Inductive tree {A: Type} : Type :=
@@ -49,24 +48,6 @@ Definition remove_option {A} (default : A) (o : option A) := match o with
 | Some x => x
 | None => default
 end.
-
-Ltac get_tuple_of_hypothesis_aux p p' k :=
-match goal with 
-| H : _ |- _ => let T := type of H in
- is_not_in_tuple p T ; try (get_tuple_of_hypothesis_aux (p, T) (p', H) k) 
-; k (p', H)
-end.
-
-Ltac get_tuple_of_hypothesis k := let H := fresh in
-assert (H : True) by (exact I) ; get_tuple_of_hypothesis_aux unit H k.
-
-
-
-Goal False -> False -> False -> False.
-intros.
-get_tuple_of_hypothesis ltac:(fun x => pose x).
-Abort.
-
 
 
 
@@ -90,19 +71,17 @@ Qed.
 Lemma search_app_snipe : forall {A: Type} {H : CompDec A} (x: A) (l1 l2: list A), search x (l1 ++ l2) = ((search x l1) || (search x l2))%bool.
 Proof.
 intros A H x l1 l2. induction l1 as [ | x0 l0 IH]. 
-- snipe. admit. admit. (*  pb de compdec sinon la preuve passe *)
+- snipe.
 - simpl. destruct (@eqb_of_compdec _ H x x0). 
- + snipe. admit. admit.
- + snipe. admit. admit.
-
-(* TODO : clears parce que scope mouline *)
-
-Abort.
+ + snipe.
+ + snipe.
+Qed.
 
 
 
 
-Lemma search_lemma : forall (A : Type) (H : CompDec A) (x: Z) (l1 l2 l3: list Z), search x (l1 ++ l2 ++ l3) = search x (l3 ++ l2 ++ l1).
+
+Lemma search_lemma : forall (A : Type) (H : CompDec A) (x: A) (l1 l2 l3: list A), search x (l1 ++ l2 ++ l3) = search x (l3 ++ l2 ++ l1).
 Proof.
 intros A H x l1 l2 l3.  rewrite !search_app.  rewrite Coq.Bool.Bool.orb_comm with (b1 := search x l3). rewrite Coq.Bool.Bool.orb_comm  with (b1 := search x l2) (b2 := search x l1 ). rewrite  Coq.Bool.Bool.orb_assoc. reflexivity.
 Qed.
@@ -110,8 +89,7 @@ Qed.
 
 Lemma snipe_search_lemma : forall (A : Type) (H : CompDec A) (x: A) (l1 l2 l3: list A), 
 search x (l1 ++ l2 ++ l3) = search x (l3 ++ l2 ++ l1).
-Proof. intros A H. snipe @search_app.
-Abort.
+Proof. intros A H. snipe @search_app. Qed.
 
 
 
