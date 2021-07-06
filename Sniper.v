de7734450d6ad9283530108d9090cf78415559b6
Chantal Keller <Chantal.Keller@lri.fr>
2021-07-06 16:43:46 +0200

    Remove all calls to verit in make

--- Sniper.v
+++ Sniper.v
@@ -71,34 +71,3 @@ Tactic Notation "scope" := scope_no_param.
 
 Tactic Notation "snipe" constr(t) := snipe_param t.
 Tactic Notation "snipe" := snipe_no_param.
-
-Section tests.
-
-Goal ((forall (A : Type) (l : list A),
-#|l| = match l with
-       | [] => 0
-       | _ :: xs => S #|xs|
-       end) -> True).
-intro H.
-eliminate_pattern_matching H.
-exact I.
-Qed.
-
-Goal ((forall (x : nat) (a : nat) (l : list nat), 
-@hd nat x (@cons nat a l) = match (@cons nat a l) with
-| nil => x
-| y :: xs => y
-end)).
-def_and_pattern_matching_mono.
-assumption.
-Qed.
-
-Goal forall (l : list Z) (x : Z),  hd_error l = Some x -> (l <> []).
-Proof.
-interp_alg_types_context_goal. 
-def_and_pattern_matching_mono.     
-verit.
-Qed.
-
-
-End tests.
