6829ae0b3eebdad14274804b7431600c8fcdbc1d
lduboisd <lduboisd@inria.fr>
2022-01-14 11:09:35 +0100

    instantiated terms generated in elpi

--- subterms.v
+++ subterms.v
@@ -1,7 +1,9 @@
 From elpi Require Import elpi.
 
-Elpi Command Collect_subterms.
 
+
+
+Elpi Command Collect_subterms.
 Elpi Accumulate File "subterms.elpi".
 Elpi Accumulate lp:{{
   main [trm Term] :- subterms Term L, coq.say L.
@@ -23,9 +25,34 @@ fix length (l : list A) {struct l} : nat  :=
   match l with
   | nil => 0
   | (_ :: l')%list => S (length l')
-  end). 
+  end).
+
+Definition toto := fun A : Type =>
+fix length (l : list A) : nat  :=
+  match l with
+  | nil => 0
+  | (_ :: l')%list => S (length l')
+  end.
+
+Print toto.
+
+Elpi Collect_subterms (toto).
 
 (* TODO : struct *)
+
+Elpi Tactic tata.
+Elpi Accumulate File "subterms.elpi".
+
+Elpi Accumulate lp:{{
+  solve (goal _ _ Ty _ _ as G) GL :- subterms Ty R, coq.say R.
+
+}}.
+Elpi Typecheck.
+
+Goal toto = toto. 
+unfold toto. elpi tata.
+Abort. 
+
 
 Elpi Command Collect_subterms_type.
 
@@ -35,28 +62,30 @@ Elpi Accumulate lp:{{
 }}.
 Elpi Typecheck.
 
+Elpi Accumulate File "subterms.elpi".
 Elpi Collect_subterms_type (Prop).
 Elpi Collect_subterms_type (fun x : Prop => Prop).
 Elpi Collect_subterms_type ((fun x : Type => Prop) Prop).
 Elpi Collect_subterms_type (nat).
 Elpi Collect_subterms_type (fun x : nat => x).
-Elpi Collect_subterms_type (fun A : Type =>
-fix length (l : list A) {struct l} : nat  :=
-  match l with
-  | nil => 0
-  | (_ :: l')%list => S (length l')
-  end).
+Elpi Collect_subterms_type (forall A : Type, nat -> unit).
 
-Ltac pose_no_name k := pose k.
+Ltac pose_list l := 
+match l with 
+| nil => idtac
+| cons ?x ?xs => pose x ; pose_list xs
+end. 
 (* TODO : pourquoi ça ne marche pas avec le pose de Ltac ?*)
 (* TODO : unquoter la liste des sous termes de type Type et la poser *)
 
-Elpi Tactic pose_subterms_of_goal.
+Elpi Tactic instantiate_with_subterms_type_type_of_goal.
+Elpi Accumulate File "subterms.elpi".
+Elpi Accumulate File "instantiate.elpi".
 Elpi Accumulate lp:{{
 
-  solve (goal _ _ Ty _ _ as G) GL :-
-    
-    coq.ltac.call "pose_no_name" [trm Ty] G GL.
+  solve (goal _ _ Ty _ [trm T] as G) _ :-
+    subterms_type Ty L, instantiate_list T L R,
+    coq.say R. 
 
 }}.
 Elpi Typecheck.
@@ -64,7 +93,8 @@ Elpi Typecheck.
 
 
 Goal forall A : Type, nat -> unit.
-elpi pose_subterms_of_goal.
+elpi instantiate_with_subterms_type_type_of_goal (forall x: Type, x = x).
 Abort.
+
 
 
