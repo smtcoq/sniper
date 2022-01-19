a0668184fed012dbc38c624e76a6e8648c39e6c5
lduboisd <lduboisd@inria.fr>
2022-01-19 16:21:09 +0100

    test to make elpi terms interact with the local context

--- subterms.v
+++ subterms.v
@@ -70,22 +70,44 @@ Elpi Collect_subterms_type (nat).
 Elpi Collect_subterms_type (fun x : nat => x).
 Elpi Collect_subterms_type (forall A : Type, nat -> unit).
 
-Ltac pose_list l := 
-match l with 
-| nil => idtac
-| cons ?x ?xs => pose x ; pose_list xs
-end. 
-(* TODO : pourquoi ça ne marche pas avec le pose de Ltac ?*)
-(* TODO : unquoter la liste des sous termes de type Type et la poser *)
+Ltac pose_alias t := pose t. 
+
+Elpi Tactic pose_list.
+Elpi Accumulate lp:{{
+  solve (goal _ _ _ _ [trm X | L] as G) GL :-
+    coq.ltac.call "pose_alias" [trm X] G GL, pose_list L.
+  solve (goal _ _ _ _ [] as G) GL :-
+    coq.ltac.call "idtac" [] G GL.
+
+}}.
+Elpi Typecheck.
+
+Elpi Tactic create_new_goal.
+Elpi Accumulate lp:{{
+
+  solve (goal _ _ _ _ [trm H] as G) GL :-
+    std.assert-ok! (coq.elaborate-ty-skeleton H _ H1) "cut formula illtyped",
+    refine (app[(fun `new_hyp` H1 x\ G1_ x), G2_]) G GL,
+    coq.say GL.
+
+}}.
+Elpi Typecheck.
+
+
+Tactic Notation "pose_list" uconstr(l) := elpi pose_list ltac_term:(l).
+
+(* TODO : un prédicat qui construit récursivement le nouveau but, 
+et la tactique ne fait qu'un refine *)
+
 
 Elpi Tactic instantiate_with_subterms_type_type_of_goal.
 Elpi Accumulate File "subterms.elpi".
 Elpi Accumulate File "instantiate.elpi".
 Elpi Accumulate lp:{{
 
-  solve (goal _ _ Ty _ [trm T] as G) _ :-
+  solve (goal _ _ Ty _ [trm T] as G) GL :-
     subterms_type Ty L, instantiate_list T L R,
-    coq.say R. 
+    coq.ltac.call "pose_list" [trm {{ unit }}] G GL. 
 
 }}.
 Elpi Typecheck.
@@ -93,6 +115,7 @@ Elpi Typecheck.
 
 
 Goal forall A : Type, nat -> unit.
+pose_list unit.
 elpi instantiate_with_subterms_type_type_of_goal (forall x: Type, x = x).
 Abort.
 
