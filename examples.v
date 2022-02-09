d9b755656363a2d5eeb4a0235fa696e46e0ac9b7
Chantal Keller <Chantal.Keller@lri.fr>
2022-02-09 15:28:24 +0100

    Examples and tests for the installed version of Sniper by default

--- examples.v
+++ examples.v
@@ -10,11 +10,10 @@
 (**************************************************************************)
 
 
-(* If you have installed Sniper, change this line into `Require Import Sniper.Sniper`. *)
-Require Import Sniper.
+From Sniper Require Import Sniper.
 Require Import String.
 Require Import ZArith.
-Require Import tree.
+From Sniper Require Import tree.
 Require Import Bool.
 Require Import Coq.Lists.List.
 Import ListNotations.
--- tests.v
+++ tests.v
@@ -10,8 +10,7 @@
 (**************************************************************************)
 
 
-(* If you have installed Sniper, change this line into `Require Import Sniper.Sniper`. *)
-Require Import Sniper.
+From Sniper Require Import Sniper.
 Require Import String.
 Require Import ZArith.
 Require Import Bool.
