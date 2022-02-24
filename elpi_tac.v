ecdc90b07ac7c5e6c33e242f0fb961f26f228093
lduboisd <lduboisd@inria.fr>
2022-02-24 08:17:47 +0100

    tests on tactic

--- elpi_tac.v
+++ elpi_tac.v
@@ -22,6 +22,65 @@ Ltac apply2 H := apply H.
 
 Ltac clear2 H := clear H.
 
+Elpi Tactic test.
+Elpi Accumulate File "utilities.elpi".
+Elpi Accumulate File "subterms.elpi".
+Elpi Accumulate lp:{{
+
+pred type_global i: term, o: term.
+  type_global (global (indt I)) Ty :- coq.env.indt I _ _ _ Ty _ _.
+
+pred codomain i:term, o:term.
+  codomain (prod Na Ty F) R :- !, pi x\ decl x Na Ty => codomain (F x) R. 
+  codomain T T.
+
+pred is_not_prop i: term, o: diagnostic.
+  is_not_prop T ok :- coq.unify-leq T {{Prop}} (error S).
+  is_not_prop T (error "the term is Prop").
+
+pred codomain_not_prop i: term, o: diagnostic.
+codomain_not_prop T ok :- codomain T U, is_not_prop U ok.
+
+pred find_instantiated_params i: term, o: (list (pair term (list term))).
+    find_instantiated_params (fun N Ty F) L :- !, find_instantiated_params Ty R1,
+        pi x\ decl x N Ty => find_instantiated_params (F x) R2, append_nodup  R1 R2 L.
+    find_instantiated_params (prod N Ty F) L :- !, find_instantiated_params Ty R1,
+        pi x\ decl x N Ty => find_instantiated_params (F x) R2, append_nodup  R1 R2 L.
+    find_instantiated_params (let N Ty V F) R :- !, find_instantiated_params Ty R1,
+        pi x\ def x N Ty V => find_instantiated_params (F x) R2, append_nodup R1 R2 R.
+    find_instantiated_params (match T U L) R :- find_instantiated_params T R1, 
+        std.map L find_instantiated_params R2,
+        std.flatten R2 R3,
+        append_nodup  R1 R3 R.
+    find_instantiated_params (fix Na _ Ty F) R :- !, find_instantiated_params Ty R1,
+        pi x\ decl x Na Ty => find_instantiated_params (F x) R2,
+        append_nodup R1 R2 R.
+    find_instantiated_params (app [(global G)|X]) [(pr (global G) R)] :- 
+    type_global (global G) Ty, codomain_not_prop Ty ok, %TODO one single call to coq.env.indt
+    get_number_of_parameters (global G) NB,
+        std.take NB X R.
+    find_instantiated_params (app L) R :- std.map L find_instantiated_params R1, std.flatten R1 R.
+    find_instantiated_params _ [].
+
+pred find_instantiated_params_in_list i: (list term), o: (list (pair term (list term))).
+    find_instantiated_params_in_list [X | XS] L :- find_instantiated_params X R1, 
+    find_instantiated_params_in_list XS R2, append_nodup R1 R2 L.
+    find_instantiated_params_in_list [] [].
+
+
+  solve (goal Ctx _ TyG _ L as G) GL :- 
+    collect_hypotheses_from_context Ctx HL HL1, polymorphic_hypotheses HL1 HL2,
+    find_instantiated_params_in_list [TyG|HL] Inst, coq.say Inst.
+  
+
+
+}}.
+Elpi Typecheck.
+
+Goal (forall (A : Type) (l : list A), A = A) -> (1 + 1 = 2) -> (forall (A : Type)
+(l: list A), l= l).
+intros. elpi test.
+
 Elpi Tactic elimination_polymorphism.
 Elpi Accumulate File "utilities.elpi".
 Elpi Accumulate File "instantiate.elpi".
