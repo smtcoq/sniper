759c32cb971e3bdda2106451d95820ca83ac35d0
lduboisd <lduboisd@inria.fr>
2022-06-21 15:40:51 +0200

    file demo ok

--- examples/demo.v
+++ examples/demo.v
@@ -8,83 +8,79 @@ Require Import Bool.
 Require Import ZArith.
 Import ListNotations.
 
-
-(* Section demo_definitions_trakt.
-
- Variable A : Type.
- Variable HA : CompDec A.
-
-(*  Variable length2 : list A -> Z.
-
- Variable app_def_nil_bool: forall (l : list A), eqb_of_compdec _ ([] ++ l) l.
- Variable app_def_cons_bool: forall  (x: A) (l l': list A), eqb_of_compdec _
-    ((x :: l) ++ l') (x :: (l ++ l')).
- Variable length2_def_nil_bool : eqb_of_compdec _ (length2 (@nil A)) 0%Z.
- Variable length2_def_cons_bool : forall (x: A) (l : list A),
-    eqb_of_compdec _ (length2 (x :: l)) (Z.add 1%Z (length2 l)).
-
-  Lemma app_length_bool (l l' : list A) : 
-  eqb_of_compdec _ (length2 (l ++ l')) (length2 l + length2 l')%Z.
-  Proof. induction l ; verit_bool. Qed. *)
-
-(*  Variable app_def_nil: forall (l : list A), [] ++ l = l.
- Variable app_def_cons: forall  (x: A) (l l': list A), (x :: l) ++ l' = x :: (l ++ l').
- Variable length_def_nil : length (@nil A) = 0.
- Variable length_def_cons : forall (x: A) (l : list A), length (x :: l) = S (length l).
-
- Lemma app_length (l l' : list A) : length (l ++ l') = (length l + length l')%nat.
-  Proof. induction l ; verit. Qed. *)
-
-Lemma app_length (l l' : list A) : length (l ++ l') = (length l + length l')%nat.
-  Proof. induction l ; snipe. Qed.
-
-End demo_definitions_trakt. *)
+Tactic Notation "get_projs_in_variables" := get_projs_in_variables unit.
 
 Section demo.
 
  Variable A : Type.
  Variable HA : CompDec A.
- Variable HlA : CompDec (list A).
-(*  Lemma HlA : CompDec (list A). auto with typeclass_instances. Qed. *)
+ Lemma HlA : CompDec (list A). auto with typeclass_instances. Qed.
+ Lemma inh : A. Proof. apply Inh. Qed.
+
+ Theorem app_eq_unit (x y:list A) (a:A) :
+      x ++ y = [a] -> x = [] /\ y = [a] \/ x = [a] /\ y = [].
+  Proof.
+    destruct x as [|a' l]; [ destruct y as [|a' l] | destruct y as [| a0 l0] ];
+      simpl.
+    intros H; discriminate H.
+    left; split; auto.
+    intro H; right; split; auto.
+    generalize H.
+    generalize (app_nil_r l); intros E.
+    rewrite -> E; auto.
+    intros H.
+    injection H as [= H H0].
+    assert ([] = l ++ a0 :: l0) as H1 by auto.
+    apply app_cons_not_nil in H1 as [].
+  Qed.
 
 
-(*  Variable app_def_nil_bool: forall (l : list A), eqb_of_compdec HlA (app [] l) l.
+(* Variable app_def_nil_bool: forall (l : list A), eqb_of_compdec HlA (app [] l) l.
  Variable app_def_cons_bool: forall
  (x: A) (l l': list A), eqb_of_compdec HlA ((x :: l) ++ l') (x :: (l ++ l')).
  Variable list_inj_bool: forall (x x' : A) (l l' : list A),
-  (eqb_of_compdec HlA (x :: l) (x :: l')) ---> (eqb_of_compdec HA x x') && (eqb_of_compdec HlA l l').
+  (eqb_of_compdec HlA (x :: l) (x' :: l')) ---> (eqb_of_compdec HA x x') && (eqb_of_compdec HlA l l').
  Variable list_disj_bool: forall (x : A) (l : list A), negb (eqb_of_compdec HlA [] (x::l)).
  Variable proj_21 : A -> list A -> A.
  Variable proj_22 : list A -> list A -> list A. 
  Variable gen_list_bool : forall (l : list A), (eqb_of_compdec HlA l []) ||
-  (eqb_of_compdec HlA l ((proj_21 inh l) :: (proj_22 [] l))). *)
+  (eqb_of_compdec HlA l ((proj_21 inh l) :: (proj_22 [] l))). 
 
-(* Theorem app_eq_unit_bool:
+Theorem app_eq_unit_bool:
     forall (x y: list A) (a:A),
       (eqb_of_compdec HlA (x ++ y) [a]) ---> 
       (((eqb_of_compdec HlA x []) && (eqb_of_compdec HlA y [a])) || 
       ((eqb_of_compdec HlA x [a]) && (eqb_of_compdec HlA y []))).
-  Proof. verit_bool. Qed. *)
+  Proof. verit_bool. Qed.  *)
 
- Variable app_def_nil: forall (l : list A), [] ++ l = l.
+(*  Variable app_def_nil: forall (l : list A), [] ++ l = l.
  Variable app2_def_cons: forall
  (x: A) (l l': list A),  ((x :: l) ++ l') = (x :: (l ++ l')).
  Variable list_inj : forall (x x' : A) (l l' : list A),
-  (x :: l) = (x :: l') -> x = x' /\ l=l'.
+  (x :: l) = (x' :: l') -> x = x' /\ l=l'.
  Variable list_disj: forall (x : A) (l : list A), ~ [] = (x::l).
  Variable proj_21' : A -> list A -> A.
  Variable proj_22' : list A -> list A -> list A. 
  Variable gen_list : forall (inh : A) (l : list A), l = [] \/
-  l = (proj_21' inh l) :: (proj_22' [] l).
+  l = (proj_21' inh l) :: (proj_22' [] l). *)
 
 Theorem app_eq_unit_auto :
     forall (x y: list A) (a:A),
       x ++ y = a :: nil -> x = [] /\ y = [a] \/ x = [a] /\ y = [].
-  Proof. snipe. Qed.
-
-
+  Proof.
+intros x y a. 
+get_def app.
+expand_hyp app_def; clear app_def.
+eliminate_fix_hyp H; clear H.
+eliminate_dependent_pattern_matching H0.
+interp_alg_types (list).
+elimination_polymorphism.
+get_projs_in_variables. verit.
+Undo. snipe. Qed. 
 
 End demo.
+
+
 
 
 
