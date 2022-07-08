807cecd3348f6d5e9ed06314491aa01d3d709ff5
lduboisd <lduboisd@inria.fr>
2022-07-08 15:30:12 +0200

    generation and proof of statements

--- theories/indrel.v
+++ theories/indrel.v
@@ -125,24 +125,35 @@ binded by a dependent product
 (so used somwhere in the term after their introduction), and stop whenever we 
 meet a non-dependent product *)
 
-Ltac infer_nb_vars T := 
-let n := fresh "n" in 
-epose (n := ?[n_evar] : nat) ;
+Ltac intros_n n :=
+match n with
+| 0 => idtac
+| S ?n' => let H := fresh in intro H ; intros_n n'
+end.
+
+Ltac infer_nb_vars T npars := 
+let n := fresh "n" in let _ := match goal with _ => 
+epose (?[n] : nat) ;
 let Hfalse := fresh "Hfalse" in
-assert (Hfalse : False -> T) by 
-(let Hf := fresh in intro Hf ; 
+assert (Hfalse : False -> T) by (let Hf := fresh in intro Hf ;  intros_n npars ; 
+(
 let rec tac_rec k := tryif (let H := fresh "H" in intro H ; 
 match goal with
 | |- context C[H] => idtac end ) 
 (* checks if the new variable is used in the goal, otherwise it is a non-dependent product *)
 then
-(tac_rec (S k)) else (instantiate (n_evar := k)) 
-in tac_rec constr:(0) ;  destruct Hf) ; clear Hfalse.
+(tac_rec (S k)) else (instantiate (n := k))
+in tac_rec constr:(0)) ; destruct Hf) ; clear Hfalse end in let u := eval unfold n in n in u.
 
 Goal False.
-infer_nb_vars (forall n m k, add (S n) m k -> add n m k).
-infer_nb_vars (forall n m k, add n m k).
-infer_nb_vars (forall n k, 1=1 -> 2=2 -> add n 0 k).
+let x := 
+infer_nb_vars (forall n m k, add (S n) m k -> add n m k) 0 in pose x.
+let x :=
+infer_nb_vars (forall n m k, add n m k) 0 in pose x. 
+let x :=
+infer_nb_vars (forall n k, 1=1 -> 2=2 -> add n 0 k) 0 in pose x.
+let x :=
+infer_nb_vars (forall (A : Type) (a : A) (l :list A), Add a l (a::l)) 2 in pose x.
 Abort.
 
 
@@ -280,13 +291,19 @@ apply Acc_intro. unfold Wf.MR. intros.
 
  Admitted. *)
 
+Fixpoint skipn_forall (t : term) (n : nat) : term := 
+match t, n with
+| tProd Na u v, S n => skipn_forall v n 
+| _, _ => t
+end.
+
 Fixpoint inv_principle_one_constructor_fuel
 (t: term) (npars : nat) (nb_vars : nat) (l1 : list term) (n : nat) :=
 match n with
 | 0 => impossible_term_reif
 | S n =>
 match t, nb_vars with
-| tProd na Ty u, (S n') => tApp <% ex %> [Ty ; tLambda na Ty (inv_principle_one_constructor_fuel u npars n' l1 n)]
+| tProd na Ty u, (S n') => tApp <% ex %> [hole ; tLambda na hole (inv_principle_one_constructor_fuel u npars n' l1 n)]
 | tProd na Ty u, 0 => tApp <% @and %> [Ty ; inv_principle_one_constructor_fuel (subst10 (tRel 0) u) npars 0 l1 n]
 | t', 0 => inv_under_binders t' npars l1
 | _, _ => impossible_term_reif
@@ -295,7 +312,8 @@ end.
 
 Definition inv_principle_one_constructor t npars nb_vars l1 := 
 let fuel := size t in
-inv_principle_one_constructor_fuel t npars nb_vars l1 fuel.
+let t' := skipn_forall t npars in
+inv_principle_one_constructor_fuel t' npars nb_vars l1 fuel.
 
 Fixpoint mkIff (t : term) (t' : term) :=
 match t with
@@ -318,10 +336,35 @@ match l_ty_cstr, list_nb_vars with
 end
 in mkIff t (aux l_ty_cstr npars list_nb_vars l1).
 
+Fixpoint mkProd_list_ty (t : term) (T : term) :=
+match T with
+| tProd na Ty u  => mkProd hole (mkProd_list_ty t u)
+| _ => t
+end.
+
+Definition app_to_args (I : term) (n : nat) :=
+match n with
+| S n' => 
+let l := Rel_list n 0 in
+tApp I l
+| 0 => I
+end.
+
+Definition create_applied_term (t: term) (T : term) (nb_args : nat) (npars : nat)
+:= 
+let l := Rel_list nb_args 0 in
+(mkProd_list_ty (tApp t l) T, List.skipn npars l).
+
 Section test.
 
 Definition c1 := <% forall n, add 0 n n %>.
 Definition c2 := <% forall n m k, add n m k -> add (S n) m (S k) %>.
+Definition add_app := <% forall n m k, add n m k %>.
+
+Definition c1' := <%forall (A : Type) (a : A) (l : list A), Add a l (a :: l) %>.
+
+Print c1'.
+
 
 Fixpoint add_prod_nat (n : nat) (t : term) :=
 match n with
@@ -329,13 +372,151 @@ match n with
 | S n' => mkProd <% nat %> (add_prod_nat n' t)
 end.
 
+Fixpoint add_prod_hole (n : nat) (t : term) :=
+match n with
+| 0 => t
+| S n' => mkProd <% hole %> (add_prod_hole n' t)
+end.
+
 Definition test_add0 := add_prod_nat 3 (inv_principle_one_constructor c1 0 1 [tRel 3 ; tRel 2 ; tRel 1]).
 Definition test_addS := add_prod_nat 3 (inv_principle_one_constructor c2 0 3 [tRel 5 ; tRel 4 ; tRel 3]).
 
+Definition test_Add0 := add_prod_hole 4 (inv_principle_one_constructor c1' 2 1 [tRel 2 ; tRel 1]).
+
+Unset Strict Unquote Universe Mode.
+
 MetaCoq Unquote Definition test_add0_unq := test_add0.
+Print test_add0_unq.
 MetaCoq Unquote Definition test_addS_unq := test_addS.
 
+Compute test_Add0.
+
+Definition goal_Add0 := <% forall (A : Type) (a : A) (l : list A)
+(l' : list A), exists l'' : list A, l = l'' /\ l' = (a::l'') %>.
+
+Print goal_Add0.
+
+
+MetaCoq Unquote Definition test_Add0_unq := test_Add0. *)
+
+
+
+Definition test_add := 
+(inv_principle_all_constructors [c1 ; c2] 0 [1 ; 3] add_app [tRel 2 ; tRel 1 ; tRel 0]).
+
+MetaCoq Unquote Definition test_add_unq := test_add.
+
 End test.
+
+Fixpoint list_fst_snd {A B C : Type} (l : list (A*B*C)) :=
+match l with
+| [] => nil
+| x :: xs => x.1.2 :: list_fst_snd xs
+end.
+
+Ltac compute_nb_vars I_reif l acc npars :=
+lazymatch l with
+| ?x :: ?xs => let t_unq := metacoq_get_value (tmUnquoteTyped Prop (subst10 I_reif x)) in
+let n := ltac:(infer_nb_vars t_unq npars) in compute_nb_vars I_reif xs (n :: acc) npars
+| [] => let acc_rev := eval cbv in (List.rev acc) in acc_rev
+end.
+
+Fixpoint compute_nb_args_reif T n :=
+match T with
+| tProd _ _ U => compute_nb_args_reif U (S n)
+| _ => n
+end.
+
+Ltac compute_nb_args T :=
+let T' := metacoq_get_value (tmQuote T) in
+let nb_args := eval cbv in (compute_nb_args_reif T' 0) in nb_args. 
+
+Ltac clear_evars n := 
+lazymatch n with
+| 0 => idtac 
+| S ?n' => match goal with
+  | [H : nat |- _]  => clear H
+           end ; clear_evars n'
+end.
+
+Ltac remove_opt x :=
+let x' := eval cbv in x in
+match x' with
+| Some ?y => y
+| None => fail
+end.
+
+Ltac right_or_left_auto :=
+first [left ; repeat eexists ; subst ; auto ; reflexivity | 
+right ; first [right_or_left_auto | repeat eexists ; subst ; auto ] ].
+
+Ltac destruct_or :=
+match goal with
+| [H: ?A\/?B |- _] => destruct H
+end.
+
+Ltac destruct_exists :=
+match goal with
+| [H: exists x, ?H' |- _] => destruct H
+end.
+
+Ltac destruct_and :=
+match goal with
+| [H: ?A/\?B |- _] => destruct H
+end.
+
+
+Goal forall H0 H1 H2 : nat,
+add H0 H1 H2 <->
+(exists n0 : nat, H0 = 0 /\ H1 = n0 /\ H2 = n0) \/
+(exists n0 m k : nat,
+   add n0 m k /\ H0 = S n0 /\ H1 = m /\ H2 = S k).
+Proof.
+intros; split ; 
+[intro H; inversion H; right_or_left_auto | 
+intros; repeat destruct_or; repeat destruct_exists; repeat destruct_and; subst; constructor; try assumption].
+Qed.
+
+
+Ltac prove_inv_principle_auto :=
+intros; split ; 
+[let H:= fresh in intro H; inversion H; right_or_left_auto | 
+intros; repeat destruct_or; repeat destruct_exists; repeat destruct_and; subst; constructor; try assumption].
+
+
+
+(* TODO factorize with having the constructors in the context *) 
+Ltac inversion_principle I := 
+let I_reif_rec := metacoq_get_value (tmQuoteRec I) in 
+let I_reif := eval cbv in I_reif_rec.2 in 
+let T := type of I in 
+let T_reif := metacoq_get_value (tmQuote T) in
+let x0 := eval cbv in (get_info_params_inductive I_reif I_reif_rec.1) in 
+let x1 := remove_opt x0 in
+let npars := eval cbv in x1.1 in
+let nb_args := compute_nb_args T in idtac nb_args ;
+let list_constr_opt := eval cbv in (get_constructors_inductive I_reif I_reif_rec.1) in 
+let I_reif_app := eval cbv in (app_to_args I_reif npars) in
+lazymatch list_constr_opt with
+| Some ?list_constr0 => let list_constr := eval cbv in (list_fst_snd list_constr0) in 
+                       let l := ltac:(compute_nb_vars I_reif list_constr (@nil nat) npars) in idtac l ;
+                       let len := eval cbv in (Datatypes.length l) in 
+                       clear_evars len ;  
+                       let p := eval cbv in (create_applied_term I_reif T_reif nb_args npars) in 
+                       let t := eval cbv in p.1 in 
+                       let l1 := eval cbv in p.2 in
+                       let inv_principle_reif := eval cbv in
+                       (inv_principle_all_constructors (List.map (fun x => subst10 I_reif x) list_constr) npars l t l1) in
+                      idtac inv_principle_reif ;
+                      let inv_principle := metacoq_get_value (tmUnquoteTyped Prop inv_principle_reif)
+                      in let H := fresh "Hinv" in assert (H : inv_principle) by (prove_inv_principle_auto)                  
+| None => fail "wrong argument"
+end.
+
+Goal False -> forall (n: nat), False. intros H n.
+inversion_principle add.
+inversion_principle @Add.
+
 
 
 
