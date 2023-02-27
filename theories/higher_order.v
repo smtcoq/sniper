2f6c012ed9b9231c112316773cc4b3b2e86450fb
lduboisd <l_dubois_de_prisque@hotmail.com>
2023-02-27 17:24:59 +0100

    first tactic : name of anonymous hypotheses

--- theories/higher_order.v
+++ theories/higher_order.v
@@ -1,7 +1,8 @@
 Require Import utilities.
 From elpi Require Import elpi.
 
-Ltac mypose t := pose t.
+Ltac mypose t := let Na := fresh "f" in pose t as Na; fold Na. (*TODO fold in all hyps except
+the self refering one *)
 
 Elpi Tactic name.
 
@@ -9,13 +10,22 @@ Elpi Accumulate File "elpi/higher_order.elpi".
 Elpi Accumulate File "elpi/utilities.elpi".
 Elpi Accumulate lp:{{
 
-  solve (goal _ _ _ _ [trm T1]) _ :- get_anonymous_funs_trm T1 L, coq.say L.
+  pred mypose_list i: list term, i: goal, o: list sealed-goal.
+  mypose_list [X|XS] G [G'|GL] :- coq.ltac.call "mypose" [trm X] G [G'],
+    coq.ltac.open (mypose_list XS) G' GL.
+  mypose_list [] _ _.
+
+  solve (goal Ctx _ TyG _ _ as G) GL :- ctx_to_trms Ctx Trms, 
+    get_anonymous_funs_list [TyG|Trms] Lfun, mypose_list Lfun G GL.
 
 }}.
 Elpi Typecheck.
 
-Goal False.
-pose (x := 1).
-elpi name (forall (x : nat), (fun (y : nat) => y) = 1).
+Require Import List.
+
+Lemma bar : forall (A B C : Type) (l : list A) (f : A -> B) (g : B -> C), 
+map g (map f l) = map (fun x => g (f x)) l.
+intros.
+elpi name. Abort.
 
 
