502c681de6b7abe1eccdeff68cc5e1e0842793b0
lduboisd <l_dubois_de_prisque@hotmail.com>
2023-03-08 17:51:38 +0100

    pose the functions in local context

--- theories/higher_order.v
+++ theories/higher_order.v
@@ -15,7 +15,7 @@ Elpi Accumulate lp:{{
     coq.ltac.open (mypose_list XS) G' GL.
   mypose_list [] _ _.
 
-  solve (goal Ctx _ TyG _ _ as G) GL :- ctx_to_trms Ctx Trms, 
+  solve (goal Ctx _ TyG _ _ as G) GL :- ctx_to_tys Ctx Trms, 
     get_anonymous_funs_list [TyG|Trms] Lfun, mypose_list Lfun G GL.
 
 }}.
@@ -35,15 +35,52 @@ Elpi Accumulate File "elpi/utilities.elpi".
 Elpi Accumulate File "elpi/subterms.elpi".
 Elpi Accumulate lp:{{
 
-  solve (goal Ctx _ TyG _ _ as G) GL :-  ctx_to_hyps Ctx Trms, names Na,
+  shorten coq.ltac.{ open, set-goal-arguments }.
+
+
+  pred select_args_type_funs i: list term, o: list term.
+    select_args_type_funs [X | XS] [X |RS] :- (coq.typecheck X {{ Type }} ok ; coq.typecheck X {{ lp:_A -> lp:_B}} ok), select_args_type_funs XS RS. 
+    select_args_type_funs _ []. 
+
+  pred trm_and_args_type_funs i: list (pair term (list term)), o: list (pair term (list term)).
+    trm_and_args_type_funs [pr X Y | XS] [pr X L| RS] :- select_args_type_funs Y L, trm_and_args_type_funs XS RS.
+    trm_and_args_type_funs [] [].
+
+  pred term_to_instance i: goal-ctx, i: int, i: term, o: instance.
+    term_to_instance [decl X _ _|_] I X (var_context I).
+    term_to_instance [def X _ _ _|_] I X (var_context I).
+    term_to_instance [_|XS] I T R :- I' is I + 1, term_to_instance XS I' T R.
+    term_to_instance [] I T (concrete_type T). 
+
+  pred term_to_instance_pr i: goal-ctx, i: (list (pair term (list term))), o: (list (pair term (list instance))).
+    term_to_instance_pr Ctx [pr X Y |L] [pr X Y' | R] :- term_to_instance_pr Ctx L R, std.map Y (term_to_instance Ctx 0) Y'.
+    term_to_instance_pr _ [] [].
+
+  pred mypose_list i: list (pair term (list instance)), i: goal, o: list sealed-goal.
+  mypose_list [pr X L |XS] (goal Ctx _ _ _ _ as G) GL :- std.rev Ctx Ctx',
+    std.map L (instance_to_term Ctx') L', 
+    coq.ltac.call "mypose" [trm (app [X | L'])] G [G'],
+    coq.ltac.open (mypose_list XS) G' GL.
+  mypose_list [] _ _.
+
+
+  solve (goal Ctx _ TyG _ _ as G) GL :- ctx_to_hyps Ctx Trms, names Na,
     subterms_list_and_args [TyG|Trms] Na Subs,
-    std.filter Subs (x\ fst x Y, contains_prenex_ho_ty Y, prenex_ho1_ty Y) L, coq.say L.
+    std.filter Subs (x\ fst x X, contains_prenex_ho_ty X, prenex_ho1_ty X) L, trm_and_args_type_funs L L', std.rev Ctx Ctx', 
+term_to_instance_pr Ctx' L' L'', mypose_list L'' G GL.
 
 }}.
 Elpi Typecheck.
 Lemma bar : forall (A B C : Type) (l : list A) (f : A -> B) (g : B -> C), 
 map g (map f l) = map (fun x => g (f x)) l.
 intros.
-elpi prenex_higher_order.
+pose (h := fun (x: A) => g (f x)).
+ fold h. elpi prenex_higher_order. (* TODO Pb : elpi change silently the name of the variables when we change the goal 
+use coq.ltac.set-goal-arguments Args G G1 G1wArgs and see 
+https://lpcic.github.io/coq-elpi/tutorial_coq_elpi_tactic.html#setting-arguments-for-a-tactic*)
+
+
+(* TODO filtrer les arguments de type produit dans la liste des arguments 
+du terme d'ordre sup *) 
 
 
