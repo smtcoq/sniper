c1bf5d6203b71096569c4210cc0441359057ee55
lduboisd <l_dubois_de_prisque@hotmail.com>
2023-08-02 17:46:59 +0200

    progress on fixpoints elpi version

--- theories/elimination_fixpoints_elpi.v
+++ theories/elimination_fixpoints_elpi.v
@@ -1,17 +1,68 @@
 From elpi Require Import elpi.
 Require Import List.
 Import ListNotations.
+
+From Ltac2 Require Import Ltac2.
+Set Default Proof Mode "Classic".
+
+Ltac intros_destructn n := 
+lazymatch n with
+| 0 => let x := fresh in intro x; destruct x
+| S ?n' => let H := fresh in intro H; intros_destructn n'
+end.
+
+Ltac myrewrite H1 H2 := setoid_rewrite H1 in H2.
+
+Ltac mypose x := pose x.
+
+Goal (forall (A : Type) (B : Type) (l : list A) (l' : list B), l = l).
+intros_destructn 3. Abort.
+
+Ltac myassert x n := assert x by (intros_destructn n ; reflexivity).
+
+Elpi Tactic eliminate_fix_hyp.
+Elpi Accumulate File "elpi/eliminate_fix.elpi".
+Elpi Accumulate File "elpi/subterms.elpi".
+Elpi Accumulate File "elpi/utilities.elpi".
+Elpi Accumulate lp:{{
+
+  solve ((goal _ _ _ _ [trm H1, trm H2]) as G) GL :- 
+
+    subst_anon_fix H1 H2 H3, coq.ltac.call "mypose" [trm H3] G GL.
+}}.
+
+Elpi Typecheck. (*     %coq.typecheck H TyH ok, 
+    %subterms_fix TyH L,*)
+
+
+Print length.
+Goal False. 
+elpi eliminate_fix_hyp (fun A : Type =>
+fix length (l : list A) {struct l} : nat :=
+  match l with
+  | [] => 0
+  | _ :: l' => S (length l')
+  end) (length). elpi eliminate_fix_hyp (fix add (n m : nat) {struct n} : nat :=
+  match n with
+  | 0 => m
+  | S p => S (add p m)
+  end) (Nat.add).
+
+
+
+% ==> pour chaque F (fix dans la liste des fix)
+
+% globals_in_goal Ctx L, std.filter L (x\ const_reduces_to x F) L'
+
 Print length.
 
-Elpi Command toto.
-Elpi Accumulate File "elpi/eliminate_fix.elpi".
-Elpi Accumulate lp:{{
-  main [trm T1, trm T2] :- mkEq T1 T2 R, coq.say R.
-}}.
-Elpi Typecheck.
-
-Elpi toto (length) (length).
-
+Goal forall (A : Type) (l : list A), False.
+intros. elpi toto (fun A : Type =>
+fix length (l : list A) {struct l} : nat :=
+  match l with
+  | [] => 0
+  | _ :: l' => S (length l')
+  end).
 
 
 Lemma test : forall (A : Type) (l : list A), 
