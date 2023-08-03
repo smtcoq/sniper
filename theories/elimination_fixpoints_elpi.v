73371a7a04910fe73011226ffd446f0006a0a837
lduboisd <l_dubois_de_prisque@hotmail.com>
2023-08-03 12:57:08 +0200

    working version of tactic

--- theories/elimination_fixpoints_elpi.v
+++ theories/elimination_fixpoints_elpi.v
@@ -2,21 +2,22 @@ From elpi Require Import elpi.
 Require Import List.
 Import ListNotations.
 
-From Ltac2 Require Import Ltac2.
-Set Default Proof Mode "Classic".
-
 Ltac intros_destructn n := 
 lazymatch n with
 | 0 => let x := fresh in intro x; destruct x
 | S ?n' => let H := fresh in intro H; intros_destructn n'
 end.
 
+(* TODO : best rewriting to handle other situations. 
+The problem is the automatic conversion made by setoid rewrite *)
+ 
 Ltac myrewrite Ty := 
 repeat match goal with
-| H1 : ?Ty1 |- _ => constr_eq Ty Ty1 ; idtac Ty1;
-             match reverse goal with
-             | H2 : ?T |- _ => idtac T; setoid_rewrite H2 in H1 ; clear H2
-              end
+| H1 : ?Ty1 |- _ => 
+  constr_eq Ty Ty1 ;
+  match goal with
+    | H2 : ?T |- _ => setoid_rewrite H2 in H1 at 2 ; clear H2
+    end
 end.
 
 
@@ -45,8 +46,8 @@ Elpi Accumulate lp:{{
     gen_eqs [] _ [].
 
     pred assert_list_rewrite i: term, i: list (pair term int), i: goal, o: list sealed-goal.
-    assert_list_rewrite H [pr Hyp I | XS] G GL :- 
-      int_to_term I I',
+    assert_list_rewrite H [pr Hyp I | XS] G GL :-
+      int_to_term I I', coq.say I', coq.say {coq.term->string Hyp},
       coq.ltac.call "myassert" [trm Hyp, trm I'] G [G1 | _],
       coq.ltac.open (coq.ltac.call "myrewrite" [trm H]) G1 [G2 | _],
       coq.ltac.open (assert_list_rewrite H XS) G2 GL.
@@ -54,24 +55,44 @@ Elpi Accumulate lp:{{
 
 
   solve ((goal Ctx _ _ _ [trm H]) as G) GL :-
-    globals_const_in_goal Ctx Glob, !, 
-    coq.typecheck H TyH ok, 
-    subterms_fix TyH L, !, 
+    globals_const_in_goal Ctx Glob, !, coq.say Glob,
+    coq.typecheck H TyH ok,
+    subterms_fix TyH L, !, coq.say L,
     gen_eqs L Glob R,
     assert_list_rewrite TyH R G GL.
 
 }}.
 
-Elpi Typecheck. (*     %coq.typecheck H TyH ok, 
-    %subterms_fix TyH L,*)
+Elpi Typecheck.
 
+Section test.
 
-Print length.
+Variable toto : nat -> nat.
+
 Goal False -> False. intros. 
-assert (H1 : forall n m, id (Nat.add n m) =
+assert (H0 : (forall (A : Type) (l : list A), 
+length l = (fix length (l : list A) : nat :=
+  match l with
+  | [] => 0
+  | _ :: l' => S (length l')
+  end) l)) by reflexivity. elpi eliminate_fix_hyp (H0).
+assert (H1 : (forall (A : Type) (l : list A), 
+length l = (fix length (l : list A) : nat :=
+  match l with
+  | [] => 0
+  | _ :: l' => S (length l')
+  end) l) -> False -> True) by (intros H1 HFalse; destruct HFalse).
+elpi eliminate_fix_hyp (H1).
+assert (H2 : forall n m, toto (Nat.add n m) =
 (fix add (n m : nat) :=
   match n with
   | 0 => m
   | S p => S (add p m)
-  end) n m) by reflexivity.
-elpi eliminate_fix_hyp (H1).
+  end) n m) by admit.
+elpi eliminate_fix_hyp (H2).
+Abort. 
+
+End test.
+
+Tactic Notation "eliminate_fix_hyp" constr(H) ltac(k) :=
+elpi eliminate_fix_hyp H ; k H.
