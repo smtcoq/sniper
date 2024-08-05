cd52f4017d1bb3e5b9865f2fd83c157731309afc
tomaz1502 <tomgm1502@gmail.com>
2024-08-05 16:51:18 +0200

    Add example for pose_case.

--- theories/pattern_matching_goal.v
+++ theories/pattern_matching_goal.v
@@ -12,6 +12,11 @@ Section Examples.
 
 Set Default Proof Mode "Classic".
 
+(* This did not work with fold (automatic reduction), but works with rewrite *)
+Goal match O with O => 42 | S _ => 41 end = 42.
+  pose_case (match O with O => 42 | S _ => 41 end).
+  reflexivity.
+Qed.
 (* pose_case does not work here (but regular scope works) -> we have to avoid lambdas? *)
 Goal forall x : nat , ((fun y => (match y with O => 42 | _ => 41 end)) x) = 41.
   intro x.
