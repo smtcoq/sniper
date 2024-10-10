69dfa0282218bfa3220c28475f36a419e9d96c26
tomaz1502 <tomgm1502@gmail.com>
2024-10-10 16:18:41 +0200

    remove logs in sig_expand

--- theories/sig_expand_elpi.v
+++ theories/sig_expand_elpi.v
@@ -14,14 +14,8 @@ Elpi Accumulate lp:{{
 
   smart_sig_expand I O :-
     sigfull I, !,
-    coq.term->string I Is,
-    coq.say "smart_sig_expand sigfull: Is = " Is,
-    sig_expand I O,
-    coq.term->string O Os,
-    coq.say "smart_sig_expand sigfull: Os = " Os.
-  smart_sig_expand I I :-
-    coq.term->string I Is,
-    coq.say "smart_sig_expand sigless: Is = " Is.
+    sig_expand I O.
+  smart_sig_expand I I.
 
   sig_expand I O :-
     coq.reduction.lazy.whd I Ir,
@@ -29,36 +23,29 @@ Elpi Accumulate lp:{{
 
 % There probably is a more direct algorithm that simultaneously checks whether there is a `sig` inside the term and
 % expands. Chantal's idea: as we traverse the tree, remember which constructors we went through and rebuild then when
-% we find a `sig`.
-% Another approach would be to understand how to use memoization
+% we find a `sig`. Another approach would be to understand how to use memoization
   %sig_expand_rec X Y :- name X, !, X = Y, !. % avoid loading "replace x x" at binders
-  sig_expand_rec (global _ as C) C :- coq.say "global", !.
-  sig_expand_rec (pglobal _ _ as C) C :- coq.say "pglobal", !.
-  sig_expand_rec (sort _ as C) C :- coq.say "sort", !.
-  sig_expand_rec (fun N T F) (fun N T1 F1) :- coq.say "fun", !,
+  sig_expand_rec (global _ as C) C :- !.
+  sig_expand_rec (pglobal _ _ as C) C :- !.
+  sig_expand_rec (sort _ as C) C :- !.
+  sig_expand_rec (fun N T F) (fun N T1 F1) :- !,
     smart_sig_expand T T1, pi x\ decl x _ T => smart_sig_expand (F x) (F1 x).
-  sig_expand_rec (let N T B F) (let N T1 B1 F1) :- coq.say "let", !,
+  sig_expand_rec (let N T B F) (let N T1 B1 F1) :- !,
     smart_sig_expand T T1, smart_sig_expand B B1, pi x\ decl x _ T => smart_sig_expand (F x) (F1 x).
-  sig_expand_rec (prod N T F) (prod N T1 F1) :- coq.say "prod", !,
+  sig_expand_rec (prod N T F) (prod N T1 F1) :- !,
     smart_sig_expand T T1, (pi x\ decl x _ T => smart_sig_expand (F x) (F1 x)).
-  sig_expand_rec (app L as T) (app L1) :-
-    coq.term->string T Ts,
-    coq.say "app: " Ts, !,
-    %(F X1 X2 :- smart_sig_expand B X1 X2) =>
+  sig_expand_rec (app L) (app L1) :-
     std.map L smart_sig_expand L1.
-  sig_expand_rec (fix N Rno Ty F) (fix N Rno Ty1 F1) :- coq.say "fix", !,
+  sig_expand_rec (fix N Rno Ty F) (fix N Rno Ty1 F1) :- !,
     smart_sig_expand Ty Ty1, pi x\ decl x _ Ty => smart_sig_expand (F x) (F1 x).
-  sig_expand_rec (match T Rty B) (match T1 Rty1 B1) :- coq.say "match", !,
+  sig_expand_rec (match T Rty B) (match T1 Rty1 B1) :- !,
     smart_sig_expand T T1, smart_sig_expand Rty Rty1,
-    %(F X1 X2 :- smart_sig_expand BB X1 X2) =>
     std.map B smart_sig_expand B1.
-  sig_expand_rec (primitive _ as C) C :- coq.say "primitive", !.
-  sig_expand_rec (uvar M L as X) W :- coq.say "uvar", var X, !,
-    %(F X1 X2 :- smart_sig_expand B X1 X2) =>
+  sig_expand_rec (primitive _ as C) C :- !.
+  sig_expand_rec (uvar M L) W :- !,
    std.map L smart_sig_expand L1, coq.mk-app-uvar M L1 W.
   % when used in CHR rules
-  sig_expand_rec (uvar X L) (uvar X L1) :- coq.say "uvar2",
-    %(F X1 X2 :- smart_sig_expand B X1 X2) =>
+  sig_expand_rec (uvar X L) (uvar X L1) :-
       std.map L smart_sig_expand L1.
 
   solve (goal _ _ _ _ [str S, trm P] as G) GL :-
